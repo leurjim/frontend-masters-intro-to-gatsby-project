075a521c2d49b3a873b21a18cec216a4d4b566f5
leurjim <shaleao@icloud.com>
2021-10-08 08:25:05 -0500

    agregando site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,4 +5,5 @@ module.exports = {
     description: 'Frontend Masters Intro to Gatsby course projects',
     image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
   },
+  plugins: ['gatsby-plugin-react-helmet'],
 };
